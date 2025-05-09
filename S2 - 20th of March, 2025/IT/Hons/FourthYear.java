73029c8d08fe9eaf648d5604e3012e223eac0ae3
Deena Shafi <deenashafi25@gmail.com>
2025-05-10 01:12:10 +0530

    Update FourthYear.java

--- S2 - 20th of March, 2025/IT/Hons/FourthYear.java
+++ S2 - 20th of March, 2025/IT/Hons/FourthYear.java
@@ -1,7 +1,7 @@
-package IT.Hons;
+package IT.Hons; //Specify the package name with its path to which the file belongs.
 
 public class FourthYear{
-	public void display(){
+	public void display(){ //Method
 		System.out.println("Hi from class FourthYear in package IT.Hons");
 	}
 } 
