86af59ce3c784e8e7668476831ce9cc2e2e8276d
Deena Shafi <deenashafi25@gmail.com>
2025-05-10 01:03:45 +0530

    Update App.java

--- S2 - 20th of March, 2025/App.java
+++ S2 - 20th of March, 2025/App.java
@@ -1,15 +1,15 @@
-import IT.*;
+import IT.*; //Allow access to all the files under the package IT.
 import IT.Hons.*;
 import AMC.*;
 
 class App{
 	public static void main(String[] args){
-		IT.FirstYear it1=new IT.FirstYear();
+		IT.FirstYear it1=new IT.FirstYear();//Create an instance of class FirstYear in package IT. 
 		FourthYear it4=new FourthYear();
-		AMC.FirstYear amc1=new AMC.FirstYear();
+		AMC.FirstYear amc1=new AMC.FirstYear();//By prefixing the package name, prevent naming conflicts. 
 		SecondYear amc2=new SecondYear();
 
-		it1.display();
+		it1.display();//Calls the method belonging to class FirstYear of package IT.
 		it4.display();
 		amc1.display();		
 		amc2.display();
