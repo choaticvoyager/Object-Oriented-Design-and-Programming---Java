9b8728cefc3ff71e5290bd7ee5dd11267a251137
Deena Shafi <deenashafi25@gmail.com>
2025-05-10 01:09:55 +0530

    Update FirstYear.java

--- S2 - 20th of March, 2025/IT/FirstYear.java
+++ S2 - 20th of March, 2025/IT/FirstYear.java
@@ -1,6 +1,6 @@
-package IT;
+package IT; //Specify the package name to which the file belongs.
 
-public class FirstYear{
+public class FirstYear{ //Method
 	public void display(){
 		System.out.println("Hi from class FirstYear in package IT");
 	}
