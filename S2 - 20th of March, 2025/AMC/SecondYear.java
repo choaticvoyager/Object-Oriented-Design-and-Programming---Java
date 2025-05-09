e6e3304b6caa997fc322ccbd82d6d72fbbe0bbb9
Deena Shafi <deenashafi25@gmail.com>
2025-05-10 01:09:02 +0530

    Update SecondYear.java

--- S2 - 20th of March, 2025/AMC/SecondYear.java
+++ S2 - 20th of March, 2025/AMC/SecondYear.java
@@ -1,6 +1,6 @@
-package AMC;
+package AMC; //Specify the package name to which the file belongs.
 
-public class SecondYear{
+public class SecondYear{ //Method
 	public void display(){
 		System.out.println("Hi from class SecondYear in package AMC");
 	}
