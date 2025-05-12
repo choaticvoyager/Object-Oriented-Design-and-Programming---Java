aed90d93e9a86a2e9330dba28b0ba28ae16c415b
Deena Shafi <deenashafi25@gmail.com>
2025-05-12 22:10:09 +0530

    Update Person.java

--- S3 - 21th of March, 2025/Invoice App/human/Person.java
+++ S3 - 21th of March, 2025/Invoice App/human/Person.java
@@ -1,5 +1,5 @@
-package human;
+package human;//Specify the package name human to which the file belongs.
 
-public class Person extends Human{
-	public String name="Alijendro";
+public class Person extends Human{ //Declare a public class named Person that inherits from the Human class. 
+	public String name="Alijendro"; //Declares a String variable name and initialize it.
 }
