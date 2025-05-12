03a7a458e4bd0dfd80e59444024d9ae242a5b60f
Deena Shafi <deenashafi25@gmail.com>
2025-05-12 21:38:27 +0530

    Update App.java

--- S3 - 21th of March, 2025/Invoice App/App.java
+++ S3 - 21th of March, 2025/Invoice App/App.java
@@ -1,10 +1,10 @@
-import human.*;
+import human.*;//Allow access to all the files under the package human.
 
 class App{
-	public static void main(String[] args){
+	public static void main(String[] args){//Main method
 		System.out.println("Welcome to Invoice App");
-		Person p1=new Person();
-		p1.talk("Hi from Angelo");
-		System.out.println("Name: "+p1.name);
+		Person p1=new Person();//Create an instance of class human in package human.
+		p1.talk("Hi from Angelo");//Calls the method belonging to class human of package IT.
+		System.out.println("Name: "+p1.name);//Calls the variable belonging to the class Person with the message.
 	}
 }
