bd3c3ffa1c47611fed20626f89c8de92d03d2459
Deena Shafi <deenashafi25@gmail.com>
2025-05-12 21:44:00 +0530

    Update Human.java

--- S3 - 21th of March, 2025/Invoice App/human/Human.java
+++ S3 - 21th of March, 2025/Invoice App/human/Human.java
@@ -1,7 +1,8 @@
-package human;
+package human;//Specify the package name human to which the file belongs.
 
 public class Human{
-	public void talk(String word){
-		System.out.println("Human says: "+word);
+	public void talk(String word){ //A public method named talk that takes a parameter word of type String.
+		System.out.println("Human says: "+word); // Displays the message inside print() followed by the value of the word parameter.
+
 	}
 }
