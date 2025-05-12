c8090c5dcdf85a2a29ba206aa2684eced94276e5
Deena Shafi <deenashafi25@gmail.com>
2025-05-12 20:12:21 +0530

    Update App.java

--- S3 - 21th of March, 2025/App.java
+++ S3 - 21th of March, 2025/App.java
@@ -1,13 +1,13 @@
-import a.*;
-import b.*;
+import a.*; //Allow access to all the files under the package a.
+import b.*; ////Allow access to all the files under the package b.
 
 class App{
-	public static void main(String[] args){
-		a.Test a1= new a.Test();
-		b.Test b1= new b.Test();
+	public static void main(String[] args){ //Main method
+		a.Test a1= new a.Test(); //Create an instance of class Test in package a. 
+		b.Test b1= new b.Test(); //Create an instance of class Test in package b.
 		
-		a1.print();
+		a1.print(); //Calls the method belonging to class Test of package a
 		a1.display();
-		b1.print();
+		b1.print(); //Calls the method belonging to class Test of package b
 	}
 }
