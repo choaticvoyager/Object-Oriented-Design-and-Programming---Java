5c9a2a2943b2725fbca55df7f0c5e45a7cbc0219
Deena Shafi <deenashafi25@gmail.com>
2025-05-12 20:15:08 +0530

    Update App.java

--- S2 - 20th of March, 2025/App.java
+++ S2 - 20th of March, 2025/App.java
@@ -1,6 +1,6 @@
 import IT.*; //Allow access to all the files under the package IT.
-import IT.Hons.*;
-import AMC.*;
+import IT.Hons.*; //Allow access to all the files under the package Hons of IT
+import AMC.*; //Allow access to all the files under the package AMC.
 
 class App{
 	public static void main(String[] args){ //Main method
@@ -10,8 +10,8 @@ class App{
 		SecondYear amc2=new SecondYear();
 
 		it1.display();//Calls the method belonging to class FirstYear of package IT.
-		it4.display();
-		amc1.display();		
-		amc2.display();
+		it4.display();//Calls the method belonging to class FourthYear of package IT.
+		amc1.display();//Calls the method belonging to class FirstYear of package AMC.	
+		amc2.display();//Calls the method belonging to class SecondYear of package AMC.
 	}
 }
